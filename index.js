f6b4c50e9b773f3287788cd9f7201e2cf0e6facf
RafaelGoulartB <rafagoulartb@gmail.com>
2019-10-18 20:56:25 -0300

    Adding view engine and static folder

--- index.js
+++ index.js
@@ -1,6 +1,13 @@
 const express = require('express')
 const app = express()
+const hbs = require('express-handlebars')
 const path = require('path')
 const env = require('./config/env')[app.get('env')]
+
+// View Configs
+app.engine('handlebars', hbs())
+app.set('view engine', 'handlebars')
+app.set('views', path.join(__dirname, 'views'))
+app.use(express.static(path.join(__dirname, 'public')))
 
 app.listen(env.PORT, () => console.log(`Server is running on https://localhost:${env.PORT}`))
