4b17df8167079aab9838c6ea68dfbe3006bdd959
RafaelGoulartB <rafagoulartb@gmail.com>
2020-02-03 07:54:50 -0300

    Fix Language Switch

--- routes/changeLanguage.js
+++ routes/changeLanguage.js
@@ -3,6 +3,7 @@ const router = require('express').Router()
 module.exports = () => {
   router.post('/', (req, res) => {
     req.session.language = req.body.lang
+    res.redirect('/')
   })
 
   return router
